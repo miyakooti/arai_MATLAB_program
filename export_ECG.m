820b19e6f969b9df895166b8e0ad7bbd680c3dc1
arai <niimaru09@gmail.com>
2022-12-29 01:34:56 +0900

    csv出力ファイルを作成

--- export_ECG.m
+++ export_ECG.m
@@ -1,1 +1,9 @@
+% 事前にrawdataを読み込んでおく。８はECG_CH
 ecg_data=rawdata(:,8);
+
+% num2cell()により、配列データをcell配列に変換できる
+Vname = {'ECG'};
+data = [num2cell(ecg_data)];
+C = [Vname;data];
+
+writecell(C,'ECG_kumakura_rest.csv')% 出力
