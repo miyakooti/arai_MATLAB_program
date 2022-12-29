ced2d71ee145ac637ef8f7fed512ac4dd8f92ae3
arai <niimaru09@gmail.com>
2022-12-29 13:06:59 +0900

    subjectを利用することで命名を自動化

--- export_ECG.m
+++ export_ECG.m
@@ -1,9 +1,12 @@
 % 事前にrawdataを読み込んでおく。８はECG_CH
 ecg_data=rawdata(:,8);
 
+
 % num2cell()により、配列データをcell配列に変換できる
 Vname = {'ECG'};
 data = [num2cell(ecg_data)];
 C = [Vname;data];
 
-writecell(C,'ECG_kumakura_rest.csv')% 出力
+path = "csv/ECG_"+subject+".csv";
+
+writecell(C,path)% 出力
