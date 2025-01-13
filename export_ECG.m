3f68b491f9c01fedab878181ce47b884340f97f2
arai <niimaru09@gmail.com>
2025-01-13 12:58:19 +0900

    try batch

--- export_ECG.m
+++ export_ECG.m
@@ -1,7 +1,6 @@
 % 事前にrawdataを読み込んでおく。８はECG_CH
 ecg_data=rawdata(:,8);
 
-
 % num2cell()により、配列データをcell配列に変換できる
 Vname = {'ECG'};
 data = [num2cell(ecg_data)];
@@ -9,10 +8,9 @@ C = [Vname;data];
 
 folder_name = findFolderName(subject);
 
-
 path = "csv/"+folder_name+"/ECG/ECG_"+subject+".csv";
 
-disp("saving to "+path);
+disp("saving to: "+path);
 
 writecell(C,path)% 出力
 
