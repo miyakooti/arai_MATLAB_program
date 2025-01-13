6f6e9c25c8dbcffd96f3d2ed95219dd31a5317dd
arai <niimaru09@gmail.com>
2025-01-13 12:29:40 +0900

    batchしようとしてる

--- export_ECG.m
+++ export_ECG.m
@@ -10,3 +10,44 @@ C = [Vname;data];
 path = "csv/temp/ECG_"+subject+".csv";
 
 writecell(C,path)% 出力
+
+
+function folder_name = findFolderName(subject)
+    if contains(subject, "kumakura")
+        folder_name = "0_kumakura";
+    elseif contains(subject, "kim")
+        folder_name = "1_kim";
+    elseif contains(subject, "souma")
+        folder_name = "2_souma";
+    elseif contains(subject, "fujii")
+        folder_name = "3_fujii";
+    elseif contains(subject, "tubota")
+        folder_name = "4_tubota";
+    elseif contains(subject, "toki")
+        folder_name = "5_toki";
+    elseif contains(subject, "mori")
+        folder_name = "6_mori";
+    elseif contains(subject, "hochi")
+        folder_name = "7_hochi";
+    elseif contains(subject, "yamada")
+        folder_name = "8_yamada";
+    elseif contains(subject, "takei")
+        folder_name = "9_takei";
+    elseif contains(subject, "tenshin")
+        folder_name = "10_tenshin";
+    elseif contains(subject, "masanori")
+        folder_name = "11_masanori";
+    elseif contains(subject, "enoki")
+        folder_name = "12_enoki";
+    elseif contains(subject, "ito")
+        folder_name = "13_ito";
+    elseif contains(subject, "yoshioka")
+        folder_name = "14_yoshioka";
+    elseif contains(subject, "chizuru")
+        folder_name = "15_chizuru";
+    end
+end
+
+
+
+
